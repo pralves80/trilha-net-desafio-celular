1493a961212642cb56b541da7a206f64d2de4f5d
Pietro Alves <pietro.alves@dedalus.eu>
2022-12-30 11:27:25 -0300

    Desafio celular full

---
+++ Models/Smartphone.cs
@@ -0,0 +1,39 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+		public string Numero { get; set; }
+		// TODO: Implementar as propriedades faltantes de acordo com o diagrama
+		private string Modelo { get; set; }
+
+		private string IMEI { get; set; }
+
+		private int Memoria { get; set; }
+
+		public Smartphone(string numero, string modelo, string imei, int memoria)
+		{// TODO: Passar os parâmetros do construtor para as propriedades
+			Numero = numero;
+			Modelo = modelo;
+			IMEI = imei;
+			Memoria = memoria;
+		}
+
+		public void Ligar()
+		{
+			Console.WriteLine("Ligando...");
+		}
+
+		public void ReceberLigacao()
+		{
+			Console.WriteLine("Recebendo ligação...");
+		}
+
+		public void Configuracao()
+		{
+			Console.WriteLine($"Número: {Numero}, Modelo: {Modelo}, Imei: {IMEI}, Memória: {Memoria}");
+		}
+
+		public abstract void InstalarAplicativo(string nomeApp);
+		
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using DesafioPOO.Models;
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("");
+
+Console.WriteLine("Nokia Smartphone:");
+Nokia n1 = new Nokia("88020294", "Modelo 1011", "12345678", 64);
+n1.Ligar();
+n1.InstalarAplicativo("Whatsapp");
+Console.WriteLine("Configuração");
+n1.Configuracao();
+
+Console.WriteLine("");
+
+Console.WriteLine("Iphone Smartphone:");
+Iphone sx = new Iphone("88011156", "Modelo SX", "98765432", 128);
+sx.ReceberLigacao();
+sx.InstalarAplicativo("Telegram");
+Console.WriteLine("Configuração");
+sx.Configuracao();
+
+Console.WriteLine("");
